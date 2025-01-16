0dc92d9a10e1ce39873aab6cc5f88200ce2f5274
yomisimie <yomisimie@gmail.com>
2025-01-16 15:28:44 +0200

    remove singleFetch

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+// declare module "@remix-run/node" {
+//   interface Future {
+//     v3_singleFetch: true;
+//   }
+// }
 
 export default defineConfig({
   base: "/animal-crossing-tracker/",
@@ -18,7 +18,7 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-        v3_singleFetch: true,
+        // v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
     }),
